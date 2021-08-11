c82ccddaee29ca826c05cc7a9ec1b1b62bb5ce32
TitaniumAlloy <34201912+TitaniumAlloy@users.noreply.github.com>
2021-08-10 20:17:58 -0400

    Create army.java

---
+++ Classes/army.java
@@ -0,0 +1,70 @@
+class Army {
+
+    public static void createArmy() {
+        new Unit("firstUnit");
+        new Unit("secondUnit");
+        new Unit("thirdUnit");
+        new Unit("fourthUnit");
+        new Unit("fifthUnit");
+        new Knight("firstKnight");
+        new Knight("secondKnight");
+        new Knight("thirdKnight");
+        new General("firstGeneral");
+        new Doctor("firstDoctor");
+    }
+
+
+    // Don't change the code below
+    static class Unit {
+        static String nameUnit;
+        static int countUnit;
+
+        public Unit(String name) {
+            countUnit++;
+            nameUnit = name;
+
+        }
+    }
+
+    static class Knight {
+        static String nameKnight;
+        static int countKnight;
+
+        public Knight(String name) {
+            countKnight++;
+            nameKnight = name;
+
+        }
+    }
+
+    static class General {
+        static String nameGeneral;
+        static int countGeneral;
+
+        public General(String name) {
+            countGeneral++;
+            nameGeneral = name;
+
+        }
+    }
+
+    static class Doctor {
+        static String nameDoctor;
+        static int countDoctor;
+
+        public Doctor(String name) {
+            countDoctor++;
+            nameDoctor = name;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        createArmy();
+        System.out.println(Unit.countUnit);
+        System.out.println(Knight.countKnight);
+        System.out.println(General.countGeneral);
+        System.out.println(Doctor.countDoctor);
+    }
+
+}
